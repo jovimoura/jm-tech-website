62d3d40d3cd1a1f26a164ee08e260d016b2b6c6d
jovimoura <joaovictors.mouraa@gmail.com>
2024-04-03 16:20:44 -0300

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,3 @@
-import { cn } from "@/lib/utils";
 import { Instagram, Linkedin } from "lucide-react";
 import { Link } from "react-router-dom";
 import { Logo } from "./Logo";
@@ -8,7 +7,7 @@ export function Footer () {
   const today = new Date();
   return (
     <>
-      <footer className='footer px-4 py-5 md:p-10 pt-12 text-black'>
+      <footer className='footer px-4 py-5 md:p-10 pt-12 border-t border-black/10 text-black'>
         <div className='flex flex-col items-start justify-start gap-4'>
           <Logo />
           <span className='max-w-[180px]'>
@@ -26,7 +25,7 @@ export function Footer () {
             </Link>
           </div>
         </div>
-        <div>
+        {/* <div>
           <span
             className={cn(
               "footer-title text-sm font-light text-primary uppercase leading-5 tracking-widest opacity-100"
@@ -43,9 +42,30 @@ export function Footer () {
           <a href={contactlink} target="_blank" className='link link-hover'>
             Contato
           </a>
-        </div>
+        </div> */}
       </footer>
       <footer className='px-5 md:px-10 py-4 border-t border-black/10'>
+        <div className="hidden md:flex items-center justify-between text-sm text-muted-foreground">
+        <span className='hidden md:flex items-center justify-center'>
+          © {today.getFullYear()} JMTech • Todos direitos reservados •
+          Desenvolvido por
+          <Link
+            to='https://jovimoura.vercel.app/'
+            target='_blank'
+            className='hover:underline'
+          >
+            John Moura
+          </Link>
+        </span> 
+        <div className="flex items-center justify-center gap-4">
+        <Link to='/privacy' className='link link-hover'>
+            Privacidade
+          </Link>
+          <Link to={contactlink} target="_blank" className='link link-hover'>
+            Contato
+          </Link>
+        </div>
+        </div>
         <span className='flex md:hidden text-xs flex-col items-center text-center justify-center w-full'>
           © {today.getFullYear()} JMTech • Todos direitos reservados{" "}
           <Link
@@ -56,17 +76,7 @@ export function Footer () {
             Desenvolvido por <span className='text-sm'>John Moura</span>
           </Link>
         </span>
-        <span className='hidden md:flex items-center justify-center w-full'>
-          © {today.getFullYear()} JMTech • Todos direitos reservados •
-          Desenvolvido por
-          <Link
-            to='https://jovimoura.vercel.app/'
-            target='_blank'
-            className='hover:underline'
-          >
-            John Moura
-          </Link>
-        </span>
+        
       </footer>
     </>
   );
